9857ee36bfd4d4b73243d03138ec26ef320fcc68
ryuta-row <oh.ryuta.dec15@gmail.com>
2021-02-02 23:41:05 +0900

    with date

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -8,7 +8,7 @@ const postsDirectory = path.join(process.cwd(), "posts");
 
 export function getSortedPostsData() {
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((filename) => {
+  const allPostsData: any = fileNames.map((filename) => {
     const pageTitle = filename.replace(/\.md/, "");
 
     const fullPath = path.join(postsDirectory, filename);
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
   });
 
   return allPostsData.sort((a, b) => {
-    if (a < b) {
+    if (a.date < b.date) {
       return 1;
     } else {
       return -1;
