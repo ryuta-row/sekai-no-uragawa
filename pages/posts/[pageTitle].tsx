966a5cf7933cb1569cb6b1a95b1b176254d0ad72
ryuta-row <oh.ryuta.dec15@gmail.com>
2021-02-03 02:12:42 +0900

    fixed typo of og

--- pages/posts/[pageTitle].tsx
+++ pages/posts/[pageTitle].tsx
@@ -8,9 +8,9 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
-        <meta name="twitter:card" content="sammary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="世界の裏側" />
-        <meta property="og:url" content={`https://sekai-no-uragawa.ryuta-row.vercel.app//posts/${postData.pageTitle}`} />
+        <meta property="og:url" content={`https://sekai-no-uragawa-3natltbaa.vercel.app/posts/${postData.pageTitle}`} />
         <meta property="og:title" content={postData.title} />
         <meta property="og:description" content={postData.description} />
         <meta property="og:image" content={postData.img} />
