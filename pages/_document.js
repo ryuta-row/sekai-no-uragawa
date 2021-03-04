b349e2a4daa57ba8040d2c61dd3dd95db35fe795
ryu-tarow <tarotarotarow@gmail.com>
2021-03-04 18:34:48 +0900

    add GAD in document.js

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,9 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <script data-ad-client="ca-pub-8024956070227392"
+            async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+          </script>
         </Head>
         <body>
           <Main />
